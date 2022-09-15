8ad1c5347e8eb1fa25e1a4c682a5daa00f0e7d32
Oliver Jarvis <oliversimonjarvis@gmail.com>
2022-09-15 14:51:40 +0200

    code now works

--- closest-points/src/cp.py
+++ closest-points/src/cp.py
@@ -1,3 +1,4 @@
+import functools
 import sys
 
 data = []
@@ -5,7 +6,9 @@ data = []
 for line in sys.stdin:
     if ":" in line or len(line.split()) < 3:
         continue
-    data.append([float(x) for x in line.split()[1:]])
+    if "EOF" in line: break
+
+    data.append(tuple([float(x) for x in line.split()[1:]]))
 
 def closest_pair(data):
     Px = sorted(data, key=lambda x: x[0])
@@ -13,17 +16,20 @@ def closest_pair(data):
     closest_pair = closest_pair_aux(Px, Py)
     return dist(closest_pair[0], closest_pair[1])
 
+@functools.cache
 def dist(p1, p2):
     return ((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)**0.5
 
 def min_dist(Px):
     min_pair = (Px[0], Px[1])
+    min_dist = dist(min_pair[0], min_pair[1])
     for i in Px:
         for j in Px:
             if i == j: 
                 continue
-            if dist(i, j) < dist(min_pair[0], min_pair[1]):
+            if dist(i, j) < min_dist:
                 min_pair = (i, j)
+                min_dist = dist(i, j)
     return min_pair
 
 def closest_pair_aux(Px, Py):
@@ -31,14 +37,23 @@ def closest_pair_aux(Px, Py):
         return min_dist(Px)
     
     Qx = Px[:len(Px)//2]
-    Qy = Py[:len(Py)//2]
     Rx = Px[len(Px)//2:]
-    Ry = Py[len(Py)//2:]
+    Qy = []
+    Ry = []
 
+    for elem in Py:
+        if elem[0] <= Qx[-1][0]:
+            Qy.append(elem)
+        else:
+            Ry.append(elem)
+    
     (q1, q2) = closest_pair_aux(Qx, Qy)
     (r1, r2) = closest_pair_aux(Rx, Ry)
 
-    gamma = min(dist(q1, q2), dist(r1, r2))
+    q1_q2_dist = dist(q1, q2)
+    r1_r2_dist = dist(r1, r2)
+
+    gamma = min(q1_q2_dist, r1_r2_dist)
     L = Qx[-1][0]
     Sy = []
 
@@ -47,16 +62,17 @@ def closest_pair_aux(Px, Py):
             Sy.append(elem)
     
     min_pairs_2 = [(0, 0),(1e7, 1e7)]
-    
+    min_dist_2 = dist(min_pairs_2[0], min_pairs_2[1])
     for i in range(len(Sy)):
         points = Sy[i + 1: min(len(Sy), (i + 1) + 15)]
         for p in points:
-            if dist(Sy[i], p) < dist(min_pairs_2[0], min_pairs_2[1]):
+            if dist(Sy[i], p) < min_dist_2:
                 min_pairs_2 = (Sy[i], p)
+                min_dist_2 = dist(Sy[i], p)
     
     if dist(min_pairs_2[0], min_pairs_2[1]) < gamma:
         return (min_pairs_2[0], min_pairs_2[1])
-    elif dist(q1, q2) < dist(r1, r2):
+    elif q1_q2_dist < r1_r2_dist:
         return (q1, q2)
     else:
         return (r1, r2)
