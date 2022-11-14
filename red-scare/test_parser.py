46d8ee5f40da28179114b88691328fae3c84aa2b
DueViktor <viktorduep@gmail.com>
2022-11-14 17:15:54 +0100

    yo it works

--- red-scare/test_parser.py
+++ red-scare/test_parser.py
@@ -1,47 +1,35 @@
-from redscare import load_file, is_directed, create_graph
+from redscare import Parser
 
 # test 1
-path = "data/common-1-20.txt"
-vertices_count, edge_count, cardinality, s, t, vertices, edges =     load_file(path)
-directed =is_directed(edges)
-
-G = create_graph(vertices,edges,directed)
-
-assert s == 'start'
-assert t == 'ender'
-assert vertices_count == 20
-assert edge_count == 0
-assert cardinality == 9
-assert G.nodes['purls']['red'] == True
-assert directed == False
+path = "common-1-20.txt"
+p = Parser(path)
+assert p.s == 'start'
+assert p.t == 'ender'
+assert p.vertices_count == 20
+assert p.edge_count == 0
+assert p.cardinality == 9
+assert p.G.nodes['purls']['red'] == True
+assert p.directed == False
 
 # test 2
-path = "data/wall-p-1.txt"
-vertices_count, edge_count, cardinality, s, t, vertices, edges =     load_file(path)
-directed =is_directed(edges)
-
-G = create_graph(vertices,edges,directed)
-
-assert s == '7'
-assert t == '0'
-assert vertices_count == 8
-assert edge_count == 8
-assert cardinality == 1
-assert G.nodes['3']['red'] == True
-assert directed == False
+path = "wall-p-1.txt"
+p = Parser(path)
+assert p.s == '7'
+assert p.t == '0'
+assert p.vertices_count == 8
+assert p.edge_count == 8
+assert p.cardinality == 1
+assert p.G.nodes['3']['red'] == True
+assert p.directed == False
 
 # test 3
-path = "data/ski-level3-1.txt"
-vertices_count, edge_count, cardinality, s, t, vertices, edges =     load_file(path)
-directed =is_directed(edges)
-
-G = create_graph(vertices,edges,directed)
-
-assert s == '0'
-assert t == '15'
-assert vertices_count == 16
-assert edge_count == 21
-assert cardinality == 5
-assert G.nodes['10']['red'] == True
-assert directed == True
+path = "ski-level3-1.txt"
+p = Parser(path)
+assert p.s == '0'
+assert p.t == '15'
+assert p.vertices_count == 16
+assert p.edge_count == 21
+assert p.cardinality == 5
+assert p.G.nodes['10']['red'] == True
+assert p.directed == True
 
